37f1e3bf988cf4d805b7de7a80352071cb69eb2e
Deataye <shahzaiba61@gmail.com>
2025-03-01 17:52:07 +0500

    jj

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -96,7 +96,7 @@ const HomeHero = () => {
       </div>
 
       {/* Right Section */}
-      <div className="relative bg-[#F4F4F4] rounded-3xl mt-12 md:mt-0">
+      <div className="relative bg-[#F4F4F4] rounded-[50px] mt-12 md:mt-0">
         {/* Professional Image */}
         <Image src="/hero-image.png" alt="Professional" width={400} height={400} className="rounded-xl" />
 
